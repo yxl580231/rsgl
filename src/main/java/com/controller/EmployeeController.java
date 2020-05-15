2c14a93f1471f6c927718cccd7a5142404212616
yxl <2871019587@qq.com>
2020-05-15 16:48:33 +0800

    项目搭建完成

---
+++ src/main/java/com/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.controller;
+
+import com.entity.Employee;
+import com.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * (Employee)表控制层
+ *
+ * @author makejava
+ * @since 2020-05-14 16:55:15
+ */
+@RestController
+@RequestMapping("employee")
+public class EmployeeController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private EmployeeService employeeService;
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("selectOne")
+    public Employee selectOne(Integer id) {
+        return this.employeeService.queryById(id);
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ASUS2018
+  Date: 2020/5/2
+  Time: 17:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>hello</h1>
+</body>
+</html>
